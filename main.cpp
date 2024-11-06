8c53eb87fe2a24d89e329a3aa1fa5d21b998d694
Dima <dmitrijsmaznov11@gmail.com>
2024-11-06 23:55:10 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "time_test.h"
+#include "test.h"
+#include "dinamicarray.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    test_unq_ptr_int();
+    test_unq_ptr_string();
+    test_unq_ptr_int_arr();
+    test_shared_ptr_int();
+    test_linked_list();
+    test_shared_ptr_arr_int();
+    test_dinamic_array();
+    compare();
 }
